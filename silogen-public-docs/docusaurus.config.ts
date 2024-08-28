0dfbac70e7441f2d41746ccb106929afc1c6eb6d
fredespi <fredrik.espinoza@gmail.com>
2024-08-28 11:38:59 +0200

    Manual update from core repo

--- silogen-public-docs/docusaurus.config.ts
+++ silogen-public-docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://silogen.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -35,7 +35,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          routeBasePath: '/',
+          routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl:
